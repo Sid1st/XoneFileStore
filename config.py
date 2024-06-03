1c9a684185136cca47665358a4778aab36db966b
Sid1st <97543215+Sid1st@users.noreply.github.com>
2024-06-03 15:42:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -73,8 +73,8 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = ""
+        URL = "https://xoneapp-sid1st-0620d415.staging.koyeb.app/"
     else:
-        URL = ""
+        URL = "https://xoneapp-sid1st-0620d415.staging.koyeb.app/"
 
 
