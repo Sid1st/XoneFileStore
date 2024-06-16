f04962ea63dd59ff9bf456589bb8c8f02c0bf19e
Sid1st <97543215+Sid1st@users.noreply.github.com>
2024-06-16 07:09:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 import re
 import os
+import random
 from os import getenv, environ
 from Script import script 
 from dotenv import load_dotenv
+from pyrogram import Client
+from pyrogram.types import InputMediaPhoto
 
 load_dotenv()
 
@@ -23,24 +26,25 @@ API_HASH = environ.get("API_HASH", "0d79d297bb8f56caed2c8f08bfc17289")
 ADMINS = int(environ.get("ADMINS", "5052476013"))
 
 # Database Information
-CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://statusringtones:fV2rGirOqMMhE1Sg@clustersubham.gs81aas.mongodb.net/?retryWrites=true&w=majority&appName=ClusterSubham")
-CDB_NAME = environ.get("CDB_NAME", "XoneFileStoreClone")
-DB_URI = environ.get("DB_URI", "mongodb+srv://subhamgupta992sg:UXRT66yXTlP7MTJt@cluster0.bza4jw6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
+CDB_NAME = environ.get("CDB_NAME", "")
+DB_URI = environ.get("DB_URI", "mongodb+srv://statusringtones:fV2rGirOqMMhE1Sg@clustersubham.gs81aas.mongodb.net/?retryWrites=true&w=majority&appName=ClusterSubham")
 DB_NAME = environ.get("DB_NAME", "XoneFileStore")
 
 
 # Bot Information
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 BOT_USERNAME = environ.get("BOT_USERNAME", "XoneFileStore_bot") # your bot username without @
-PICS = (environ.get('PICS', 'https://telegra.ph/file/d07e0216dc9610b5d232c.jpg https://telegra.ph/file/d07e0216dc9610b5d232c.jpg')).split() # Bot Start Picture
+PICS = (environ.get('PICS', ['https://telegra.ph/file/5bdceafb32c966c5b5b4c.jpg',
+                              'https://telegra.ph/file/e2515f6559c7f4da99afd.jpg',])) # Bot Start Picture
 
 # Auto Delete Information
 AUTO_DELETE = int(environ.get("AUTO_DELETE", "20")) # Time in Minutes
 AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1200")) # Time in Seconds
 
 # Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1001760664883"))
-FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1002182741200')).split()]
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1001700999955"))
+FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1002219601256' '-1002249081198')).split()]
 
 
 # File Caption Information
@@ -58,7 +62,7 @@ class Var(object):
     name = str(getenv('name', 'XoneFileStore'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001760664883'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001700999955'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
@@ -73,8 +77,8 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = "https://xonefilestore.onrender.com"
+        URL = ""
     else:
-        URL = "https://xonefilestore.onrender.com"
+        URL = ""
 
 
